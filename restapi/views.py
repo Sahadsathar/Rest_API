bead897bc8fb5f8449e0d89bcefc93402483cf79
sahad sathar <sahadsathar1997@gmail.com>
2022-12-07 07:16:53 +0500

    'serializers'

--- restapi/views.py
+++ restapi/views.py
@@ -1,6 +1,10 @@
+
+
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest.serializers import StudentSerializer
+from rest.models import Student
 
 class TestView(APIView):
     def get(self, requests, *args, **kw):
@@ -9,3 +13,10 @@ class TestView(APIView):
             'no. of years': 10,
         }
         return Response(data)
+
+    def post(self, requests, *args, **kw):
+        serializer = StudentSerializer(data = requests.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
